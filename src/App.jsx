cfbd1515313c1f799e608866bd3af99c7e3da4b8
ArmanAproyan <aaproyan@mail.ru>
2024-06-28 15:32:55 +0400

    start

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-  <input type="text" name="" id="" />
-      <div><h1>Hello</h1></div>
-      <button>BArev Aper</button>
+
     </>
   )
 }
