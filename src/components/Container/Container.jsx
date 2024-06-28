e7adec2db7852cfdc5403a8c058169e7f81db4a5
ArmanAproyan <aaproyan@mail.ru>
2024-06-28 18:46:30 +0400

    background

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const Container = () => {
+  return (
+    <div className='container'>
+        <div className="right_container"></div>
+    </div>
+  )
+}
+
+export default Container
